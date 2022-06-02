93e41db9bb550c2ad70a6ffdf025992ef01dff9b
ST_DOMAIN\Kalinevich_RS <Kalinevich_RS@st.by>
2022-06-02 14:53:26 +0300

    Folder structure

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from'./Post.module.css';
+const Post = () => {
+    return <div className={styles.item}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4HvloBhZVvCau4er4R7GJV3y0k-01b4sLiQ&usqp=CAU"></img>
+          post 1
+          <div>
+            <span>Like</span>
+          </div>
+        </div>
+
+}
+
+export default Post
