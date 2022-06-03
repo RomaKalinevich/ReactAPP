bfdeaee1cd672ead260c5df92cc38b6b4583f8e3
ST_DOMAIN\Kalinevich_RS <Kalinevich_RS@st.by>
2022-06-03 12:09:19 +0300

    use WebStorm, add Dialogs module

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
-import styles from'./Profile.module.css';
+import styles from './Profile.module.css';
+
 const Profile = () => {
-    return  <div className={styles.content}>
-    <div>
-      <img src='https://thumbs.dreamstime.com/b/%D1%88%D0%B8%D1%80%D0%BE%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B0%D0%BD%D0%BE%D1%80%D0%B0%D0%BC%D0%B0-%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B8-%D0%B0%D1%81%D1%84%D0%B0%D0%BB%D1%8C%D1%82%D0%B0-%D0%B2-%D1%81%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B9-%D0%BC%D0%B5%D1%81%D1%82%D0%BD%D0%BE%D1%81%D1%82%D0%B8-%D0%BB%D0%B5%D1%82%D0%BD%D0%B8%D0%B9-%D0%B4%D0%B5%D0%BD%D1%8C-%D0%BD%D0%B0-214241529.jpg' alt='decs'></img>
-    </div>
-    <div>
-      ava + desc
-    </div>
-    <MyPosts />
-  </div>
+    return (
+        <div>
+            <div>
+                <img
+                    src='https://thumbs.dreamstime.com/b/%D1%88%D0%B8%D1%80%D0%BE%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B0%D0%BD%D0%BE%D1%80%D0%B0%D0%BC%D0%B0-%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B8-%D0%B0%D1%81%D1%84%D0%B0%D0%BB%D1%8C%D1%82%D0%B0-%D0%B2-%D1%81%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B9-%D0%BC%D0%B5%D1%81%D1%82%D0%BD%D0%BE%D1%81%D1%82%D0%B8-%D0%BB%D0%B5%D1%82%D0%BD%D0%B8%D0%B9-%D0%B4%D0%B5%D0%BD%D1%8C-%D0%BD%D0%B0-214241529.jpg'
+                    alt='decs'>
+
+                </img>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <MyPosts/>
+        </div>
+    )
 }
 
 export default Profile
